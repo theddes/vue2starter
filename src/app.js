102bf355f24a57ca71c2551cab8f35c71e406344
theddes <timo@noprotocol.nl>
2016-08-30 16:55:20 +0200

    comment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import store from './store'
 import App from './App.vue'
 sync(store, router)
 
+// enable dev tools if we're in development
 Vue.config.debug = Vue.config.devtools = (process.env.NODE_ENV === 'development')
 
 const app = new Vue({
