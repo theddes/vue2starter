2f2bcf44ed3357bf707299b9f858b0d5bdf41dd9
theddes <timo@noprotocol.nl>
2016-08-30 16:19:57 +0200

    vue devtools/debug environment switch

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import store from './store'
 import App from './App.vue'
 sync(store, router)
 
+Vue.config.debug = Vue.config.devtools = (process.env.NODE_ENV === 'development')
+
 const app = new Vue({
     router,
     store,
